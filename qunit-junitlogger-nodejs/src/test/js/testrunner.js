9cb2a6635c272620a71dba0ee9637e6a467570e6
gustavo.andriotti <gustavo.andriotti@gmail.com>
2012-12-10 16:42:24 -0200

    Maven removal, requires retargeting output file

--- qunit-junitlogger-nodejs/src/test/js/testrunner.js
+++ qunit-junitlogger-nodejs/src/test/js/testrunner.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 QUnit = require('qunitjs');
 require('qunitjs/addons/junitlogger/junitlogger');
 
-var xmlFile = fs.createWriteStream('target/surefire/simple.xml');
+var xmlFile = fs.createWriteStream('simple.xml');
 QUnit.jUnitReport = function(data) {
 	xmlFile.write(data.xml);
 };
